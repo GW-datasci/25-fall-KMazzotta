f3dad21b0a5c26ce0d3c660eeac0de5dd541a6d7
kylamazzotta <kylamazzotta@gwu.edu>
2025-10-13 13:21:40 -0400

    added some basic summary charts

--- data/datacleaning.R
+++ data/datacleaning.R
@@ -3,7 +3,11 @@ library(readr)
 
 install.packages("tidyverse")
 install.packages("janitor")
+install.packages("ggplot2")
+library(tidyverse)
 library(janitor)
+library(ggplot2)
+
 ultras <- read_csv("ultras.csv") %>% 
   clean_names() %>%
   filter(
@@ -12,9 +16,28 @@ ultras <- read_csv("ultras.csv") %>%
          !is.na(athlete_age_category) &
          str_detect(event_name, "USA"))
 
-
-
 summary(ultras)
+
+
+# some basic plots to start
+ggplot(ultras, aes(x = athlete_gender)) +
+  geom_bar(fill = "steelblue") +
+  labs(
+    title = "Number of Athletes by Gender",
+    x = "Gender",
+    y = "Count"
+  ) +
+  theme_minimal()
+
+ggplot(ultras, aes(x = athlete_age_category)) +
+  geom_bar(fill = "darkseagreen") +
+  labs(
+    title = "Distribution of Athletes by Age Category",
+    x = "Age Category",
+    y = "Count"
+  ) +
+  theme_minimal() +
+  coord_flip()
 
 
 
